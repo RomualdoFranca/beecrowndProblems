95bf1917435673b4fb2ccd691f35950978284cb0
RomualdoFranca <cerebrodekerberuss@gmail.com>
2022-07-02 19:01:18 -0300

    Cria atributos do objeto,getters e setters, descricao do problema

--- simpleSort/src/application/Program.java
+++ simpleSort/src/application/Program.java
@@ -1,7 +1,21 @@
 package application;
 
+
+import java.util.Scanner;
+
+/*
+Read three integers and sort them in ascending order. After, print these values in ascending order,
+a blank line and then the values in the sequence as they were readed.
+Input
+The input contains three integer numbers.
+Output
+Present the output as requested above.
+*/
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
     }
 }
