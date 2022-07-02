34169c39c4928c3566b53ad6444b51c5e4d8f4e1
RomualdoFranca <cerebrodekerberuss@gmail.com>
2022-07-02 14:12:37 -0300

    Criacao dos atributos, construtor, gette e sette

--- coordinates/src/entities/CoordinatesOfAPoint.java
+++ coordinates/src/entities/CoordinatesOfAPoint.java
@@ -1,4 +1,28 @@
 package entities;
 
 public class CoordinatesOfAPoint {
+
+    private double pointX;
+    private double pointY;
+    private String quadrant;
+    public CoordinatesOfAPoint(double pointX, double pointY) {
+        this.pointX = pointX;
+        this.pointY = pointY;
+    }
+
+    public double getPointX() {
+        return pointX;
+    }
+
+    public void setPointX(double pointX) {
+        this.pointX = pointX;
+    }
+
+    public double getPointY() {
+        return pointY;
+    }
+
+    public void setPointY(double pointY) {
+        this.pointY = pointY;
+    }
 }
