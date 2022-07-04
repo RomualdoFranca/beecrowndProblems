31c2370c9706ab85ae3eec18968ab89ae0cabe8a
RomualdoFranca <cerebrodekerberuss@gmail.com>
2022-07-04 15:27:11 -0300

    apagando comentario

--- simpleSort/src/application/Program.java
+++ simpleSort/src/application/Program.java
@@ -6,7 +6,7 @@ import entities.SimpleSort;
 import java.util.Scanner;
 
 /*
-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
+
 
 Read three integers and sort them in ascending order. After, print these values in ascending order,
 a blank line and then the values in the sequence as they were readed.
